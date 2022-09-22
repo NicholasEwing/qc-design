587ee3c84d29932112b96c646fe4e564e7fd9e19
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-22 18:25:41 -0400

    Finish config for now

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Queen City Designs'
+export const SITE_DESCRIPTION = 'One-stop web solutions for small businesses.'
+export const HOMEPAGE_URL = 'https://www.qc-design.com/'
