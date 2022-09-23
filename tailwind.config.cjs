2bbb4ed5b987302f16106d8cef461673569ab22f
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-23 17:04:49 -0400

    Updates tailwind config with bg colors, adds hero anims

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,11 @@ module.exports = {
         heading: ['Outfit', 'Verdana', 'sans-serif']
       },
       backgroundImage: {
+        'gradient-fuchsia':
+          'linear-gradient(112.84deg,#46baeb .33%,#af2cff 38.23%,#f790af 65.22%,#c1abff 100%)',
         'gradient-cyan':
-          'linear-gradient(90deg,#6cd5f6 0,#f89d5c 50%,#5b6af0 100%)'
+          'linear-gradient(90deg,#6cd5f6 0,#f89d5c 50%,#5b6af0 100%)',
+        'gradient-green': 'linear-gradient(98.24deg,#cee9c1 0,#83c8de 100%)'
       }
     }
   },
