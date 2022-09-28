7e1775614fcf5d9e8b80ed52a5daf5eb92624316
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-28 11:31:49 -0400

    Makes /contact, fixes site title, moves <ContactForm /> into component

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Queen City Designs'
+export const SITE_TITLE = 'Queen City Design'
 export const SITE_DESCRIPTION = 'One-stop web solutions for small businesses.'
 export const HOMEPAGE_URL = 'https://www.qc-design.com/'
