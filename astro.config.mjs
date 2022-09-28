cfba255f037468693315e91547e5c5fa8db31fa9
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-28 13:35:53 -0400

    Adds Google Analytics via Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.qc-design.com/',
-  integrations: [sitemap(), react(), image(), tailwind()]
+  integrations: [
+    sitemap(),
+    react(),
+    image(),
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push']
+      }
+    })
+  ]
 })
